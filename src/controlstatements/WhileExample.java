6c490a71e74a0db1d1c38335d9bac28e4d969109
91733 <91733@LAPTOP-99VTF96N>
2021-10-20 19:18:03 +0530

    while loop

---
+++ src/controlstatements/WhileExample.java
@@ -0,0 +1,13 @@
+package controlstatements;
+
+public class WhileExample {
+	
+		public static void main(String[] args) {  
+		    int i=1;  
+		    while(i<=11){  
+		        System.out.println(i);  
+		    i++;  
+		    }  
+		}  
+
+}
