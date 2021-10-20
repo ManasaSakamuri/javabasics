01460e81f9347048a1569b42d72da9e062d48c1a
91733 <91733@LAPTOP-99VTF96N>
2021-10-20 19:21:37 +0530

    dowhile loop

---
+++ src/controlstatements/DoWhileExample.java
@@ -0,0 +1,12 @@
+package controlstatements;
+
+public class DoWhileExample {
+	public static void main(String[] args) {    
+	    int i=1;    
+	    do{    
+	        System.out.println(i);    
+	    i++;    
+	    }while(i<=10);    
+	}      
+
+}
