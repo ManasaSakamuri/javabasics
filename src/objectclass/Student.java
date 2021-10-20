347290deb490295f0992ff20e31a8b32a05ee7ed
91733 <91733@LAPTOP-99VTF96N>
2021-10-20 10:21:09 +0530

    student

---
+++ src/objectclass/Student.java
@@ -0,0 +1,23 @@
+package objectclass;
+
+public class Student {
+	
+	String name;
+	int age;
+	
+	static String COLLEGE_NAME = "III";
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		
+	}
+
+}
