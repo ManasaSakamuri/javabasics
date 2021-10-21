e765ea0cb605090614527796dc12fff6489ef3f5
91733 <91733@LAPTOP-99VTF96N>
2021-10-21 12:21:16 +0530

    final keyword

---
+++ src/finalkeyword/Bike9.java
@@ -0,0 +1,13 @@
+package finalkeyword;
+
+public class Bike9 {
+	
+	int speedlimit=90;//final variable  
+	 void run(){  
+	  speedlimit=400;  
+	 }  
+	 public static void main(String args[]){  
+	 Bike9 obj=new  Bike9();  
+	 obj.run();  
+	 }  
+}
